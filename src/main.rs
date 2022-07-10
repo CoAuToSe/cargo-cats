33855e2c7e6c4610992d9eed4c4206900b99dd47
CoAuToSe <Aurelien.Couthures@gmail.com>
2022-07-10 14:16:41 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,75 @@
-#![recursion_limit = "256"]
-use std::{
-    env::{self, *},
-    path::{self, *},
-};
-macro_rules! show {
-    ($a:expr) => {
-        println!("{:?}", $a);
-};
-    ($a:expr, $($b:expr),+) => {
-        print!("{}", $a);
-        show!($($b),+);
-    };
-}
+// #![recursion_limit = "256"]
+// use std::{
+//     env::{self, *},
+//     path::{self, *},
+// };
+// macro_rules! show {
+//     ($a:expr) => {
+//         println!("{:?}", $a);
+// };
+//     ($a:expr, $($b:expr),+) => {
+//         print!("{}", $a);
+//         show!($($b),+);
+//     };
+// }
+
+// fn main() {
+//     println!("Hello, world!");
+//     show!("Hello, world!");
+//     show!(args_os());
+//     show!(args());
+//     let paths = [Path::new("/bin"), Path::new("/usr/bin")];
+//     show!(join_paths(paths.iter()));
+//     println!();
+
+//     let key = "PATH";
+//     match env::var_os(key) {
+//         Some(paths) => {
+//             for path in env::split_paths(&paths) {
+//                 println!("'{}'", path.display());
+//             }
+//         }
+//         None => println!("{} is not defined in the environment.", key),
+//     }
+//     println!();
+//     let key = "HOME";
+//     match env::var_os(key) {
+//         Some(val) => println!("{}: {:?}", key, val),
+//         None => println!("{} is not defined in the environment.", key),
+//     }
+//     println!();
+//     for (key, value) in env::vars_os() {
+//         println!("{:?}: {:?}", key, value);
+//     }
+//     println!();
+//     for (key, value) in env::vars() {
+//         println!("{}: {}", key, value);
+//     }
+
+//     println!();
+//     let mut dir = env::temp_dir();
+//     println!("Temporary directory: {}", dir.display());
+// }
+use std::{env, fs, path::Path};
 
 fn main() {
-    println!("Hello, world!");
-    show!("Hello, world!");
-    show!(args_os());
-    show!(args());
-    let paths = [Path::new("/bin"), Path::new("/usr/bin")];
-    show!(join_paths(paths.iter()));
-    println!();
+    let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("build.rs");
+    if !dest_path.exists() {
+        fs::write(
+            &dest_path,
+            "use std::{env, process::Command};
 
-    let key = "PATH";
-    match env::var_os(key) {
-        Some(paths) => {
-            for path in env::split_paths(&paths) {
-                println!("'{}'", path.display());
-            }
-        }
-        None => println!("{} is not defined in the environment.", key),
+fn main() {
+    let base_dir = env::var(\"CARGO_MANIFEST_DIR\").unwrap();
+    Command::new(\"cargo\")
+        .arg(\"fmt\")
+        .current_dir(&base_dir)
+        .output()
+        .unwrap();
+}
+        ",
+        )
+        .unwrap();
     }
-    println!();
-    let key = "HOME";
-    match env::var_os(key) {
-        Some(val) => println!("{}: {:?}", key, val),
-        None => println!("{} is not defined in the environment.", key),
-    }
-    println!();
-    for (key, value) in env::vars_os() {
-        println!("{:?}: {:?}", key, value);
-    }
-    println!();
-    for (key, value) in env::vars() {
-        println!("{}: {}", key, value);
-    }
-
-    println!();
-    let mut dir = env::temp_dir();
-    println!("Temporary directory: {}", dir.display());
 }
