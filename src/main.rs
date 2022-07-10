5d2be1b81b9de600bd816378f4d3466f3d27cbba
CoAuToSe <Aurelien.Couthures@gmail.com>
2022-07-10 14:42:06 +0200

    aze

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@
 use std::{env, fs, path::Path};
 
 fn main() {
-    println!("{:?}", env::var_os("CARGO_CRATE_NAME ").unwrap());
+    println!("{:?}", env::var_os("CARGO_CRATE_NAME").unwrap());
     println!("{:?}", env::var_os("OUT_DIR").unwrap());
     let out_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("build.rs");
